0e9e9e6d5e8985295777ab15a6c06bdf8d32f1ad
joshhoegen <joshhoegen@gmail.com>
2013-09-20 16:32:53 -0400

    TRUE fullscreen

--- js/main.js
+++ js/main.js
@@ -108,20 +108,46 @@ jQuery(document).ready(function () {
 		}
             });
         },
+	requestFullScreen = function (element, callback) {
+	    // Supports most browsers and their versions.
+	    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+	
+	    if (requestMethod) { // Native full screen.
+		requestMethod.call(element);
+	    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+		var wscript = new ActiveXObject("WScript.Shell");
+		if (wscript !== null) {
+		    wscript.SendKeys("{F11}");
+		}
+	    }
+	    if (typeof callback != 'undefined') {
+		callback();
+	    }
+	},
 	fullscreen = function(full){
 	    var on = full || jQuery('sc-fullscreen').data('on'),
-		width = (jQuery(document).width()+scopeSize),
-		height = (jQuery(document).height()+scopeSize),
+		width, height,
 		total = 8;
 	    if (!on) {
-		total = (height/125)+(width/125);
-		jQuery('body').addClass('fullscreen').find('.wrapper').css({
-		    width: (width) ,
-		    height: (height-scopeSize)
+		requestFullScreen(document.body, function(){
+		    width = (jQuery(document).width()+scopeSize),
+		    height = (jQuery(document).height()+scopeSize),
+		    total = (height/125)+(width/125);
+		    jQuery('body').addClass('fullscreen').find('.wrapper').css({
+			width: (width) ,
+			height: (height-scopeSize)
+		    });
+		    addNewImages(audioCache[audioActive].image, scopeSize, total);
+		    jQuery('input[name=sc-fullscreen]').data({on: true});    
 		});
-		addNewImages(audioCache[audioActive].image, scopeSize, total);
-		jQuery('input[name=sc-fullscreen]').data({on: true});
 	    } else {
+		if (document.cancelFullScreen) {
+		    document.cancelFullScreen();
+		} else if (document.mozCancelFullScreen) {
+		    document.mozCancelFullScreen();
+		} else if (document.webkitCancelFullScreen) {
+		    document.webkitCancelFullScreen();
+		}
 		jQuery('body').removeClass('fullscreen').find('.wrapper').attr('style', '');
 		addNewImages(audioCache[audioActive].image, scopeSize, total);
 		jQuery('input[name=sc-fullscreen]').data({on: false});
