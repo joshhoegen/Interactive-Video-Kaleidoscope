75bcfe442b18ffc030232c7bd45425dfb31e3887
joshuahoegen <joshuahoegen@l000179.local>
2017-06-25 12:25:26 -0400

    Removing tagrget blank

--- src/js/jhHeader/index.js
+++ src/js/jhHeader/index.js
@@ -12,26 +12,26 @@ export default class Header extends React.Component {
         <div id="jh-header" className="data-scope">
           <div id="jh-nav" className="data-scope">
             <ul data-controller="pagesList" data-show="showMenu" className="pages data-scope">
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="http://instagram.com/joshhoegen" target="_blank" className="data-binding"><i className="fa fa-instagram" /> Instagram<span className="data-binding">- Art and life in static images</span></a>
+              <li className="pages-listing data-scope">
+                <a href="http://instagram.com/joshhoegen"><i className="fa fa-instagram" /> Instagram<span>- Art and life in static images</span></a>
               </li>
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="https://byutifu.com/trails/" target="_blank" className="data-binding"><i className="fa fa-globe" /> Js + Video = Trails <span className="data-binding">- Interactive art</span></a>
+              <li className="pages-listing data-scope">
+                <a href="https://byutifu.com/"><i className="fa fa-globe" /> Js + Video = Trails <span>- Interactive art</span></a>
               </li>
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="https://byutifu.com/" target="_blank" className="data-binding"><i className="fa fa-globe" /> Js + Video = Kaleidoscope <span className="data-binding">- Interactive art</span></a>
+              <li className="pages-listing data-scope">
+                <a href="https://byutifu.com/kaleidoscope"><i className="fa fa-globe" /> Js + Video = Kaleidoscope <span>- Interactive art</span></a>
               </li>
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="http://github.com/joshhoegen" target="_blank" className="data-binding"><i className="fa fa-github" /> Git<span className="data-binding">- Not as much stuff as I'd like, but it's there!</span></a>
+              <li className="pages-listing data-scope">
+                <a href="http://github.com/joshhoegen"><i className="fa fa-github" /> Git<span>- Not as much stuff as I'd like, but it's there!</span></a>
               </li>
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="http://linkedin.com/pub/josh-hoegen/b/a9/9b8" target="_blank" className="data-binding"><i className="fa fa-linkedin" /> LinkedIn<span className="data-binding">- Professional profile</span></a>
+              <li className="pages-listing data-scope">
+                <a href="http://linkedin.com/pub/josh-hoegen/b/a9/9b8"><i className="fa fa-linkedin" /> LinkedIn<span>- Professional profile</span></a>
               </li>
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="http://soundcloud.com/byutifu" target="_blank" className="data-binding"><i className="fa fa-soundcloud" /> Sound Cloud<span className="data-binding">- Remix of Jazz and other Retro Artists</span></a>
+              <li className="pages-listing data-scope">
+                <a href="http://soundcloud.com/byutifu"><i className="fa fa-soundcloud" /> Sound Cloud<span>- Remix of Jazz and other Retro Artists</span></a>
               </li>
-              <li data-repeat="Page in pages.page | orderBy:orderProp" className="pages-listing data-scope">
-                <a href="http://flickr.com/photos/joshhoegen" target="_blank" className="data-binding"><i className="fa fa-flickr" /> Flickr<span className="data-binding">- Visual Art</span></a>
+              <li className="pages-listing data-scope">
+                <a href="http://flickr.com/photos/joshhoegen"><i className="fa fa-flickr" /> Flickr<span>- Visual Art</span></a>
               </li>
             </ul>
           </div>
