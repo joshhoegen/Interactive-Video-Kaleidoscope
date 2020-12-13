219fd4641cf122ff871745974776a9aadcbf07e4
joshhoegen <joshhoegen@gmail.com>
2020-12-13 13:07:33 +0100

    Fixing runtime error with async

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import React from 'react' // eslint-disable-line no-unused-vars
 import { render } from 'react-dom'
 import App from './App'
